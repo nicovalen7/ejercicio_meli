643ceeef48e3fb06d1f9944fb4cd8b70662645d4
Nicolás Valentinuzzi <nicovalentinuzzi@gmail.com>
2020-08-26 19:08:58 -0300

    Estructura básica para levantar cliente

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { hot }  from 'react-hot-loader';
+import './App.scss';
+
+const App = () => (
+    <div>
+        App working
+    </div>
+);
+
+export default hot(module)(App);
